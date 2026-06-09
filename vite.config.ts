05fd04dae668685fd37419532e239b07283199aa
Vladimir Potemkin <vapotemkin@akontech.ru>
2026-06-09 14:53:18 +0300

    vite config fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'node:path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
 
 export default defineConfig({
+  server: {
+    host: true,
+  },
   plugins: [react()],
   resolve: {
     alias: {
-      '@/app':      resolve(__dirname, 'src/app'),
-      '@/pages':    resolve(__dirname, 'src/pages'),
-      '@/widgets':  resolve(__dirname, 'src/widgets'),
+      '@/app': resolve(__dirname, 'src/app'),
+      '@/pages': resolve(__dirname, 'src/pages'),
+      '@/widgets': resolve(__dirname, 'src/widgets'),
       '@/features': resolve(__dirname, 'src/features'),
       '@/entities': resolve(__dirname, 'src/entities'),
-      '@/shared':   resolve(__dirname, 'src/shared'),
+      '@/shared': resolve(__dirname, 'src/shared'),
     },
   },
-})
+});
